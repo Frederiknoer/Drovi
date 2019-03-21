41c592ef01562f773373c1c7228c9ad5a894bcc1
Patrickfp <papet15@student.sdu.dk>
2019-03-21 15:17:34 +0100

    Working subscriber and GMM

--- mini-project2/src/miniproject2/src/videoKalman.py
+++ mini-project2/src/miniproject2/src/videoKalman.py
@@ -1,22 +1,45 @@
+#!/usr/bin/python
 import rospy
 import cv2
 import numpy as np
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 
-vid = cv2.VideoCapture("../2017_06_23_dyrskuepladsen.mp4")
-fgbg = cv2.createBackgroundSubtractorMOG2()
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+
+kernel = np.ones((5,5),np.uint8)
+
+#def callback(data):
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 
-while(True):
-    ret, frame = vid.read()
+def kalman(data):
+
+    #vid = cv2.VideoCapture("../2017_06_23_dyrskuepladsen.mp4")
+    frame = CvBridge().imgmsg_to_cv2(data,'bgr8')
+
+    #print np.shape(frame)
+
+
     fgmask = fgbg.apply(frame)
-    cv2.imshow('frame',fgmask)
+    fgmask_morph = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    cv2.imshow('frame',fgmask_morph)
     k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
+
+
+def listener():
+
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("analyzed_image", Image, kalman)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
 
 
 
 
-cap.release()
-cv2.destroyAllWindows()
+
+
