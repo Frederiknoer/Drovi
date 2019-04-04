c6048fe6d12e0723522be74126d519048a5725bf
Frederiknoer <Frederikk@live.dk>
2019-04-04 14:29:27 +0200

    things done

--- mini-project2/KalmanFiterVideo.py
+++ mini-project2/KalmanFiterVideo.py
@@ -13,62 +13,81 @@ class KalmanFilterVideo:
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.speed = 0
+        self.dt = 1.0
 
-        self.f_x = KalmanFilter(dim_x=2, dim_z=1)
-        self.f_y = KalmanFilter(dim_x=2, dim_z=1)
+        self.KF = KalmanFilter(dim_x=4, dim_z=2)
+        self.KF.x = np.array([0.,0.,0.,0.])
+        self.KF.P = np.array([  [1000., 0., 0., 0.],
+                                [ 0., 1000., 0., 0.],
+                                [0., 0., 1000., 0.],
+                                [0., 0., 0., 1000.]])
 
-        self.f_x.x = np.array([[2.], [0.]])  # position, velocity
-        self.f_x.F = np.array([[1., 1.], [0., 1.]])
-        self.f_x.H = np.array([[1., 0.]])
-        self.f_x.P = np.array([[1000.,    0.],
-                        [   0., 1000.] ])
-        self.f_x.R = np.array([[5.]])
-        self.f_x.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+        self.KF.Q = np.eye(4) * .01
+        self.KF.R = np.array([  [15., 0.],
+                                [0., 15.]])
 
-        self.f_y.x = np.array([[2.], [0.]])  # position, velocity
-        self.f_y.F = np.array([[1., 1.], [0., 1.]])
-        self.f_y.H = np.array([[1., 0.]])
-        self.f_y.P = np.array([[1000.,    0.],
-                        [   0., 1000.] ])
-        self.f_y.R = np.array([[5.]])
-        self.f_y.Q = Q_discrete_white_noise(dim=2, dt=0.1, var=0.13)
+        self.KF.H = np.array([  [1.,0.,0.,0.,],
+                                [0.,0.,1.,0.,]])
+
+        self.KF.F = np.array(([ [1., self.dt, 0., 0.],
+                                [0., 1., 0., 0.],
+                                [0., 0., 1., self.dt],
+                                [0., 0., 0., 1.]]))
 
     def KalmanPrediction(self):
-        self.f_x.predict()
-        self.f_y.predict()
+        self.KF.predict()
 
     def updateValues(self, new_x_pos, new_y_pos):
-        self.f_x.update(new_x_pos)
-        self.f_y.update(new_y_pos)
+        self.KF.update([[new_x_pos], [new_y_pos]])
+        self.speed = math.sqrt(math.pow(float(self.KF.x[1]),2) + math.pow(float(self.KF.x[3]),2))
+
+class KfArray:
+    def __init__(self):
+        self.arr = []
+
+    def CheckID(self, ID, x_pos, y_pos):
+        itemExists = 0
+        for elem in arr:
+            if elem.ID == ID:
+                itemExists += 1
+                elem.updateValues(x_pos, y_pos)
+        if itemExists == 0:
+            KF = KalmanFilterVideo(ID=ID, x_pos=x_pos, y_pos=y_pos)
+            self.arr.append(KF)
+
+    def arrayPredict(self):
+        for elem in arr:
+            elem.KalmanPrediction()
+
+def drawSquares(img, x, y):
+    cv2.rectangle(img, ((x-3),(y-8)),((x+3),(y+8)))
+
+class KFros:
+    def __init__(self):
+        rospy.init_node('KF', anonymous=True)
+        self.image_sub = rospy.Subscriber("Image_BG_filtered", data, self.callback)
+        self.image_pub = rospy.Publisher("KF_position_estimate", self.KF_array, queue_size=10)
+        self.KF_array = KfArray()
+
+    def callback(self, data):
+        self.KF_array.arrayPredict()
+        if len(data) > 1:
+            self.KF_array.CheckID(data[0], data[1], data[2])
 
 
-    def calcSpeed(self):
-        self.speed = math.sqrt(math.pow(float(self.f_x.x[1]),2) + math.pow(float(self.f_y.x[1]),2))
-        return self.speed
 
 if __name__ == '__main__':
+        print("Launching Kalman filter")
+        kf = KFros()
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            print("Shutting down")
+        cv2.destroyAllWindows()
 
-    vis = np.zeros((800, 800,1), np.uint8)
-    kalmanImg = np.zeros((800, 800,1), np.uint8)
 
-    cv2.namedWindow("Kalman")
-    cv2.namedWindow("Video Feed")
 
-    KF = KalmanFilterVideo(ID=1, x_pos=0, y_pos=0)
 
-    for i in range(720):
-        x = i + random.randint(-5,20)
-        y = i + random.randint(-25,15)
-        #vis[:][:] = 0
-        vis[i+y][i+x] = 255
 
-        KF.KalmanPrediction()
-        KF.updateValues(new_x_pos=i+x, new_y_pos=i+y)
-        print(KF.calcSpeed())
 
-        #print(int(f_x.x[0]))
-        kalmanImg[int(KF.f_x.x[0]), int(KF.f_y.x[0])] = 255
-        cv2.imshow("Video Feed", vis)
-        cv2.imshow("Kalman", kalmanImg)
-        cv2.waitKey(20)
-        #vis[i][i] = 255
+    #end
