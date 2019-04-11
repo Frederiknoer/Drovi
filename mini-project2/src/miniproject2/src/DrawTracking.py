2a227d8c6874de958edd5648feed29569ef0b0fe
Frederiknoer <Frederikk@live.dk>
2019-04-11 22:40:19 +0200

    .

--- mini-project2/src/miniproject2/src/DrawTracking.py
+++ mini-project2/src/miniproject2/src/DrawTracking.py
@@ -77,7 +77,7 @@ class DrawTracking:
         time = (minutes + seconds + m_seconds)
         cv2.putText(frame, time,(75, 75), font, fontScale=5, color=np.array([0,0,255]), thickness=5)
         cv2.rectangle(frame, (620, 245), (720, 255), np.array([0, 0, 255]), thickness=2)
-        cv2.rectangle(frame, (900, 1045), (1100, 1055), np.array([0, 0, 255]), thickness=2)
+        cv2.rectangle(frame, (950, 1000), (1100, 1010), np.array([0, 0, 255]), thickness=2)
 
         for car in self.car_list:
             cv2.circle(frame, (car.x, car.y), radius, colour, thickness)
