98c4d8c36420a6895567fe4f3213ae9d19c41e17
Patrickfp <papet15@student.sdu.dk>
2019-05-09 10:05:59 +0200

    markerdetect

--- mini-project4/src/miniproject4/src/marker_detection.py
+++ mini-project4/src/miniproject4/src/marker_detection.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 import rospy
 import cv2
+from fiducial.nFoldEdges.MarkerLocator import markerTracker, MarkerPose
 import numpy as np
 from sensor_msgs.msg import Image
 from std_msgs.msg import Int16MultiArray
 from cv_bridge import CvBridge
-from miniproject2.msg import Car, Cars
 import random
 import message_filters
 import math
+
+
+
+class markerDetection:
+    def __init__(self):
+        print "hej"
+
+
+
+if __name__ == '__main__':
+    print "marker detection node started"
+    mark = markerDetection()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
