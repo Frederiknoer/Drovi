a2d55ce20181ee08462ecfe7a0e4c472291f2e3c
Patrickfp <papet15@student.sdu.dk>
2019-05-14 00:31:00 +0200

    markerdetectnode

--- mini-project4/src/miniproject4/src/marker_detection.py
+++ mini-project4/src/miniproject4/src/marker_detection.py
@@ -11,25 +11,28 @@ import message_filters
 import math
 import sys
 
-sys.path.insert(0, 'fiducial/nfoldedge/MarkerLocator/')
+sys.path.insert(0, 'fiducial/nfoldedge/')
 
-from MarkerPose import MarkerPose
-from MarkerTracker import MarkerTracker
+from MarkerLocator import MarkerPose
+from MarkerLocator import MarkerTracker
+from MarkerLocator.msg import markerpose
 
 
 class markerDetection:
     def __init__(self):
         rospy.init_node("marker_detection",anonymous=True)
 
-        tracker = MarkerTracker.MarkerTracker(
+        self.tracker = MarkerTracker.MarkerTracker(
             order=4,
             kernel_size=20,
             scale_factor=1)
         # Tell the tracker that we are looking for markers without
         # an unique orientation
-        tracker.track_marker_with_missing_black_leg = True
+        self.tracker.track_marker_with_missing_black_leg = True
 
         self.image_sub = rospy.Subscriber("hummingbird/camera_/image_raw", Image, self.callback)
+        self.markerpose_pub = rospy.Publisher("nFold_markerpose",markerpose,queue_size=10)
+        self.image_pub = rospy.Publisher("nFold_markerimage", Image, queue_size=10)
 
 
 
@@ -37,11 +40,24 @@ class markerDetection:
 
         frame = CvBridge().imgmsg_to_cv2(data, 'bgr8')
 
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+        pose = self.tracker.locate_marker(gray)
+        cv2.circle(frame, (pose.x, pose.y), 20, (0, 0, 255), 2)
+        #print(pose.quality)
 
+        markerpose_msg = MarkerPose
+        markerpose_msg.header = data.header
+        markerpose_msg.order = pose.order
+        markerpose_msg.x = pose.x
+        markerpose_msg.y = pose.y
+        markerpose_msg.theta = pose.theta
+        markerpose_msg.quality = pose.quality
 
+        self.markerpose_pub.publish(markerpose_msg)
 
-
+        self.image_pub.publish(CvBridge().cv2_to_imgmsg(frame))
 
 if __name__ == '__main__':
     print "marker detection node started"
